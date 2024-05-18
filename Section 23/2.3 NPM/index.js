974b3eadecd77a47fa9ba18f06d97e7058baebb3
Navya <navyashreer02@gmail.com>
2024-05-18 15:09:57 +0530

    commit

---
+++ Section 23/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// import superheroes from 'superheroes';
+
+// const name = superheroes.random();
+// console.log(`I am ${name}!`);
+
+import { randomSuperhero } from 'superheroes';
+
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
+
